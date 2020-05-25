f915c70ed5c3b4e50154a39829e480651dbc325d
infant23 <pavlo.domozhyrskyi@limestonedigital.co>
2020-05-25 17:38:07 +0300

    add users admin

--- test_app/users/admin.py
+++ test_app/users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+from .models import User
+
+
+class UserAdmin(DjangoUserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('app_id', 'username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_confirmed', 'is_active', 'is_staff',
+                                    'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    readonly_fields = ('app_id',)
+    list_display = ('username', 'app_id', 'first_name', 'last_name',
+                                'is_confirmed')
+    search_fields = ('username', 'email', 'app_id', 'first_name', 'last_name')
+    ordering = ('date_joined',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'is_confirmed',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
